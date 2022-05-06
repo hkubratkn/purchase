3d45e9d2286744541b6153685faf31a8cb87b04c
CASPER <hkubratkn@gmail.com>
2022-05-06 13:28:14 +0300

    Initial commit

---
+++ app/src/main/java/com/kapirti/eagle/MainActivity.kt
@@ -0,0 +1,349 @@
+package com.kapirti.eagle
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import android.app.Application
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.*
+import com.kapirti.eagle.ui.theme.EagleTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import kotlinx.coroutines.flow.catch
+import java.io.IOException
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import com.google.accompanist.pager.ExperimentalPagerApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+
+    @Inject
+    lateinit var splashViewModel: SplashViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen().setKeepOnScreenCondition {
+            !splashViewModel.isLoading.value
+        }
+
+        setContent {
+            EagleTheme {
+                val screen by splashViewModel.startDestination
+                val navController = rememberNavController()
+                SetupNavGraph(navController = navController, startDestination = screen)
+            }
+        }
+    }
+}
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "on_boarding_pref")
+class DataStoreRepository(context: Context) {
+
+    private object PreferencesKey {
+        val onBoardingKey = booleanPreferencesKey(name = "on_boarding_completed")
+    }
+
+    private val dataStore = context.dataStore
+
+    suspend fun saveOnBoardingState(completed: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.onBoardingKey] = completed
+        }
+    }
+
+    fun readOnBoardingState(): Flow<Boolean> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                val onBoardingState = preferences[PreferencesKey.onBoardingKey] ?: false
+                onBoardingState
+            }
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreRepository(
+        @ApplicationContext context: Context
+    ) = DataStoreRepository(context = context)
+
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController,
+    startDestination: String
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomeScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
+
+sealed class Screen(val route: String) {
+    object Welcome : Screen(route = "welcome_screen")
+    object Home : Screen(route = "home_screen")
+}
+
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "HOME",
+            fontSize = MaterialTheme.typography.h4.fontSize
+        )
+    }
+}
+
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun WelcomeScreen(
+    navController: NavHostController,
+    welcomeViewModel: WelcomeViewModel = hiltViewModel()
+) {
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+    val pagerState = rememberPagerState()
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            modifier = Modifier.weight(10f),
+            count = 3,
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { position ->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(1f),
+            pagerState = pagerState
+        )
+        FinishButton(
+            modifier = Modifier.weight(1f),
+            pagerState = pagerState
+        ) {
+            welcomeViewModel.saveOnBoardingState(completed = true)
+            navController.popBackStack()
+            navController.navigate(Screen.Home.route)
+        }
+    }
+}
+
+@Composable
+fun PagerScreen(onBoardingPage: OnBoardingPage) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = "Pager Image"
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = onBoardingPage.title,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .padding(top = 20.dp),
+            text = onBoardingPage.description,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 40.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxWidth(),
+            visible = pagerState.currentPage == 2
+        ) {
+            Button(
+                onClick = onClick,
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Finish")
+            }
+        }
+    }
+}
+
+
+sealed class OnBoardingPage(
+    @DrawableRes
+    val image: Int,
+    val title: String,
+    val description: String
+) {
+    object First : OnBoardingPage(
+        image = R.drawable.ic_launcher_foreground,
+        title = "Meeting",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+
+    object Second : OnBoardingPage(
+        image = R.drawable.ic_launcher_background,
+        title = "Coordination",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+
+    object Third : OnBoardingPage(
+        image = R.drawable.ic_launcher_foreground,
+        title = "Dialogue",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod."
+    )
+}
+
+class SplashViewModel @Inject constructor(
+    private val repository: DataStoreRepository
+) : ViewModel() {
+
+    private val _isLoading: MutableState<Boolean> = mutableStateOf(true)
+    val isLoading: State<Boolean> = _isLoading
+
+    private val _startDestination: MutableState<String> = mutableStateOf(Screen.Welcome.route)
+    val startDestination: State<String> = _startDestination
+
+    init {
+        viewModelScope.launch {
+            repository.readOnBoardingState().collect { completed ->
+                if (completed) {
+                    _startDestination.value = Screen.Home.route
+                } else {
+                    _startDestination.value = Screen.Welcome.route
+                }
+            }
+            _isLoading.value = false
+        }
+    }
+
+}
+
+
+
+@HiltViewModel
+class WelcomeViewModel @Inject constructor(
+    private val repository: DataStoreRepository
+) : ViewModel() {
+
+    fun saveOnBoardingState(completed: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.saveOnBoardingState(completed = completed)
+        }
+    }
+
+}
+
+@HiltAndroidApp
+class MyApplication: Application()
